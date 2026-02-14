931d23c6e3d4d9cf6a8aa17257dcb609f5387436
unknown <blackskiesdancer@gmail.com>
2026-02-14 16:57:29 +1100

    Fix asset prefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/valentines",
-  images: { unoptimized: true },
+  basePath: isProd ? "/valentines" : "",
+  assetPrefix: isProd ? "/valentines/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
